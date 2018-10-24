1bee6ea4ff9486b50ab0af4966274ba03cbf584a
hilmi-a <hilmi.a.aziz@gmail.com>
2018-10-24 13:13:29 +0700

    file prep

---
+++ App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="App">
+        Making Instagram clone
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "App"
+    };
+</script>
+
+<style lang="scss" src="./styles/app.scss">
+</style>
